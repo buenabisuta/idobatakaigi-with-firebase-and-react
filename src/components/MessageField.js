55209733d5a18cc6f6e6f9f0b5fdc409ba675ccd
buenabisuta <buenabisuta765@gmail.com>
2021-03-23 00:25:19 +0900

    focus処理を追加

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { TextField } from '@material-ui/core';
 import { pushMessage } from '../firebase';
 
-const MessageField = ({ name, setText, text }) => {
+const MessageField = ({ inputEl,name, setText, text }) => {
   const [isComposed,setIsComposed] = useState(false);
 
   return <TextField 
@@ -27,6 +27,7 @@ const MessageField = ({ name, setText, text }) => {
   }}
   value={text}
   autoFocus={true}
+  inputRef={inputEl}
 />;
 }
 
--- src/components/MessageSubmitButton.js
+++ src/components/MessageSubmitButton.js
@@ -3,11 +3,12 @@ import { pushMessage } from '../firebase';
 import { IconButton } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
 
-const MessageSubmitButton = ({name, setText, text}) => {
+const MessageSubmitButton = ({inputEl, name, setText, text}) => {
   return (
     <IconButton disabled={text === ''} onClick={() => {
       pushMessage({name: 'buena',text});
       setText('');
+      inputEl.current.focus();
     }}>
       <SendIcon />
     </IconButton>
